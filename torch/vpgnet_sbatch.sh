02cd63e6256ddcee9d20ad1feaddad6b30f2c36f
akashgokul <akashgokul@berkeley.edu>
2020-05-05 20:26:19 -0700

    testing with new data

--- torch/vpgnet_sbatch.sh
+++ torch/vpgnet_sbatch.sh
@@ -6,13 +6,16 @@
 #SBATCH --account=fc_vivelab
 #
 # Partition:
-#SBATCH --partition=savio2
+#SBATCH --partition=savio2_1080ti
 #
 # Request one node:
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #
 # Request cores (24, for example)
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
+#
+#Request GPUs
+#SBATCH --gres=gpu:2
 #
 #Request CPU
 #SBATCH --cpus-per-task=8
@@ -24,6 +27,6 @@
 #SBATCH --mail-user=akashgokul@berkeley.edu
 ## Command(s) to run (example):
 module load python
-# module load cuda/10.1
+module load cuda/10.1
 source activate /global/scratch/akashgokul/kaolin_run
-python3 main.py --root_dir /global/scratch/akashgokul/VPGNet_Yohan --csv_path /global/scratch/akashgokul/VPGNet_data/mat_paths.csv --batch_size 32 --num_epochs_vp 1 --num_epochs_general 25
+python3 main.py --root_dir /global/scratch/akashgokul/VPGNet_Yohan --csv_path /global/scratch/akashgokul/VPGNet_data/mat_paths.csv --batch_size 32 --num_epochs_vp 5 --num_epochs_general 5
