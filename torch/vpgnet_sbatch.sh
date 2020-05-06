c6b9ed7fd374ec5c8ad51fbc3e149089907d09eb
akashgokul <akashgokul@berkeley.edu>
2020-05-06 14:23:03 -0700

    testing with cpu

--- torch/vpgnet_sbatch.sh
+++ torch/vpgnet_sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=vpgnet_run_may3
+#SBATCH --job-name=vpgnet_subset
 #
 # Account:
 #SBATCH --account=fc_vivelab
@@ -26,4 +26,4 @@
 ## Command(s) to run (example):
 module load python
 source activate /global/scratch/akashgokul/kaolin_run
-python3 main.py --root_dir /global/scratch/akashgokul/VPGNet_Yohan --csv_path /global/scratch/akashgokul/VPGNet_data/mat_paths.csv --batch_size 1024 --num_epochs_vp 2 --num_epochs_general 5
+python3 main.py --root_dir /global/scratch/akashgokul/VPGNet --csv_path /global/scratch/akashgokul/VPGNet_data/mat_paths.csv --batch_size 32 --num_epochs_vp 1 --num_epochs_general 5
