75a81d6917a71a961c680f699d72e60789686b2e
akashgokul <akashgokul@berkeley.edu>
2020-05-09 23:25:18 -0700

    trying bce and transform img

--- torch/dataset.py
+++ torch/dataset.py
@@ -227,6 +227,8 @@ class VPGData(Dataset):
             transforms.ToTensor(), 
             transforms.Normalize(mean=mean, std=std)])
 
+        print(mean)
+        print(std)
         return transform(rgb_img), obj_mask, vp
 
 
