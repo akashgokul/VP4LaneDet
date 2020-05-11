93b2809fc8a27ea253f4369d1504a4b81c76f22a
akashgokul <akashgokul@berkeley.edu>
2020-05-11 00:35:55 -0700

    sbatching naive

--- torch/naivenet_sbatch.sh
+++ torch/naivenet_sbatch.sh
@@ -18,11 +18,11 @@
 #SBATCH --cpus-per-task=4
 #
 # Wall clock limit:
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=akashgokul@berkeley.edu
 ## Command(s) to run (example):
 module load python
 source activate /global/scratch/akashgokul/kaolin_run
-python3 main.py --model naive --root_dir /global/scratch/akashgokul/VPGNet_Yohan --csv_path /global/scratch/akashgokul/VPGNet_data/mat_paths.csv --batch_size 64 --num_epochs_general 2
+python3 main.py --model naive --root_dir /global/scratch/akashgokul/VPGNet_Yohan --csv_path /global/scratch/akashgokul/VPGNet_data/mat_paths.csv --batch_size 32 --num_epochs_general 2
