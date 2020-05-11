1e432879c8e3c988ed328c25b0d994b46fb8cf4e
akashgokul <akashgokul@berkeley.edu>
2020-05-10 21:02:13 -0700

    adding transform and convtranspose

--- torch/lanedetect_model.py
+++ torch/lanedetect_model.py
@@ -45,7 +45,7 @@ class LaneDetect(nn.Module):
             #Conv 8
             nn.Conv2d(4096, 64, kernel_size=1, stride=1, padding=0), 
             #Tiling
-            nn.ConvTranspose2d(128, 1, kernel_size = 8)
+            nn.ConvTranspose2d(64, 1, kernel_size = 8)
         )
         # self.vp = nn.Sequential(
         #     #Conv 7
